ea6b49904b406269e5b152413e47624b24b5c5e4
robmcnamara <robert.mcnamara@wise.com>
2024-01-11 10:29:33 +0000

    change to better theme. add description for Neotree commands

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -5,9 +5,11 @@ local opts = { noremap = true, silent = true }
 keymap.set("n", "te", ":tabedit<Return>", {silent = true})
 keymap.set("n", "<tab>", ":tabnext<Return>", opts)
 keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
 -- Split window
 keymap.set("n", "ss", ":split<Return>", opts)
 keymap.set("n", "sv", ":vsplit<Return>", opts)
+
 -- Move window
 keymap.set("n", "sh", "<C-w>h")
 keymap.set("n", "sk", "<C-w>k")
@@ -15,4 +17,6 @@ keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sl", "<C-w>l")
 
 keymap.set("n", '<leader>kl', "<cmd>Neotree toggle<cr>")
+keymap.set("n", '<leader>kk', "<cmd>Neotree source=filesystem reveal=true position=left toggle=true<cr>", {desc="Toggle Neotree: current file"})
+keymap.set("n", '<leader>kg', "<cmd>Neotree float git_status git_base=main<cr>", {desc="Show git status"})
 
