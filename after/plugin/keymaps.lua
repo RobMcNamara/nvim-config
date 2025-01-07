1e2654ae8590700b8045b4eed4fe71d553b9e75d
robmcnamara <robert.mcnamara@wise.com>
2025-01-07 16:40:14 +0000

    add terraform, change theme to be less sore on eyes

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -17,6 +17,6 @@ keymap.set("n", "sj", "<C-w>j")
 keymap.set("n", "sl", "<C-w>l")
 
 keymap.set("n", '<leader>kl', "<cmd>Neotree toggle<cr>")
-keymap.set("n", '<leader>kk', "<cmd>Neotree source=filesystem reveal=true position=left toggle=true<cr>", {desc="Toggle Neotree: current file"})
+keymap.set("n", '<leader>kk', "<cmd>Neotree source=filesystem reveal=true position=left<cr>", {desc="Toggle Neotree: current file"})
 keymap.set("n", '<leader>kg', "<cmd>Neotree float git_status git_base=main<cr>", {desc="Show git status"})
 
